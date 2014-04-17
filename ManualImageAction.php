052a3521b5a82b16074f83aac3357ff34289e214
Christopher Stebe <chris@stebe.eu>
2014-04-17 18:34:23 +0200

    update ManualImageAction -> check baseAlias

--- ManualImageAction.php
+++ ManualImageAction.php
@@ -12,21 +12,12 @@ class ManualImageAction extends CAction
 
     public function run()
     {
-        //        $cacheId = "MeridaManualWidget." . $_GET['img'];
-        //        if ($cachedItems = Yii::app()->cache->get($cacheId)) {
-        //            echo $cachedItems;
-        //        } else {
-        //            $file       = Yii::getPathOfAlias($this->baseAlias) . DIRECTORY_SEPARATOR . $_GET['img'];
-        //            $outputFile = file_get_contents($file);
-        //
-        //            Yii::app()->cache->set($cacheId, $outputFile, 0, self::getMenuCacheDependency());
-        //            echo $outputFile;
-        //        }
-        //        Yii::app()->end();
-
-        $file = Yii::getPathOfAlias($this->baseAlias) . DIRECTORY_SEPARATOR . $_GET['img'];
-        echo file_get_contents($file);
-
+        if (isset($this->baseAlias) && Yii::getPathOfAlias($this->baseAlias) !== false) {
+            $file = Yii::getPathOfAlias($this->baseAlias) . DIRECTORY_SEPARATOR . $_GET['img'];
+            echo file_get_contents($file);
+        } else {
+            echo Yii::t('manual','No baseAlias defined');
+        }
         Yii::app()->end();
     }
 }
